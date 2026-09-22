af48c22317dc53de882fc2c39c910a9f97ac4b08
DaniIFSP <a.danielle@aluno.ifsp.edu.br>
2026-09-22 18:21:02 -0300

    Debbuging

--- app/Http/Controllers/RotaController.php
+++ app/Http/Controllers/RotaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Supports\Facades\Log;
+use Illuminate\Support\Facades\Log;
 
 class RotaController extends Controller
 {
